ae15f2d19d51b0cbdcab3443c911924bdad4dfe7
PC <pc@PCs-Mac-mini.local>
2025-12-24 21:42:08 +0900

    All models set

--- L2MSS.R
+++ L2MSS.R
@@ -1,36 +1,47 @@
 set.seed(123)
 my_data <- read.csv("data.csv", header = TRUE, sep = ",")
+
+install.packages("psych") ## For EFA
 library(psych)
+
+## Split dataset for EFA and CFA
 N <- nrow(my_data)
 indices <- 1:N
 indices_EFA <- sample(indices, floor(0.5 * N))
 indices_CFA <- indices[!(indices %in% indices_EFA)]
 data_EFA <- my_data[indices_EFA, ]
 data_CFA <- my_data[indices_CFA, ]
+
+## EFA ##
+
+## Set up groupings of variables for EFA
 group5 <- c(24:33,45:66)
 efa_results <- fa(data_EFA[, group5], nfactors = 2, rotate = "geominT")
 efa_results <- fa.sort(efa_results)
 print(efa_results)
+
 biplot.psych(efa_results)
 fa.plot(efa_results)
 fa.diagram(efa_results)
 fa.parallel(data_EFA[, group5])
 
+## CFA ##
+
 install.packages("lavaan") ## For CFA
-install.packages("semPlot") ## Used for creating factor diagrams
+
 # load CFA required libraries
 library(lavaan)
-library(semPlot)
+
 
 # Define CFA models
 Group1 <- ' 
 One_factor =~ Intend1 + Intend2 + Intend3 + Intend4 + Intend5 + Intend6 + Intend7
 + Intend8 + Intend9 + Intend10 + ATLE1 + ATLE2 + ATLE3 + ATLE4 + ATLE5
 '
-# Fit model to data
+# Fit models to data
 fit <- cfa(Group1, data_CFA, std.lv = TRUE)
 summary(fit, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
-semCors(fit)
+
 
 Group2 <- ' Group2_1 =~ Comm1 + Comm2 + Comm3 + Comm4 + Culture1 + Culture2 + Culture3 + Culture4 '
 fit2 <- cfa(Group2, data_CFA, std.lv = TRUE)
@@ -40,6 +51,7 @@ Group2a <- ' Comm    =~ Comm1 + Comm2 + Comm3 + Comm4
              Comm ~~ Culture '
 fit2a <- cfa(Group2a, data_CFA, std.lv = TRUE)
 summary(fit2a, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+## Compare models
 anova(fit2, fit2a)
 
 oughtTo <- ' oneFactor =~ Ought1 + Ought2 + Ought3 + Ought4 + Ought5 + Ought6 + Ought7 + Ought8 + Ought9 + Ought10 
@@ -53,3 +65,5 @@ oughtTob <- ' Factor1 =~ Ought1 + Ought2 + Ought3 + Ought4 + Ought5 + Ought6 + O
              Factor3 =~ Prev1 + Prev2 + Prev3 + Prev4 + Prev5 + Prev6 + Prev7 + Prev8 '
 oughtTo2 <- cfa(oughtTob, data_CFA, std.lv = TRUE)
 summary(oughtTo2, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+## Compare models
+anova(oughtTo1, oughtTo2)
